2c83b07710786e528846539c0ff7d6fbb4c90201
Mateusz <mtpala@interia.pl>
2023-03-06 18:52:44 +0100

    dodanie flagi do sterowania możliwością przeklinania na serwerze

--- src/main/java/com/mateo9x/carbot/handler/MessageHandler.java
+++ src/main/java/com/mateo9x/carbot/handler/MessageHandler.java
@@ -1,6 +1,7 @@
 package com.mateo9x.carbot.handler;
 
 import com.mateo9x.carbot.CarBot;
+import com.mateo9x.carbot.config.Configuration;
 import com.mateo9x.carbot.game.CarMeterGame;
 import com.mateo9x.carbot.game.GameType;
 import com.mateo9x.carbot.game.FunFactGame;
@@ -8,6 +9,9 @@ import com.mateo9x.carbot.game.QuizGame;
 import com.mateo9x.carbot.validator.ForbiddenWordValidator;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+
+import java.io.IOException;
 
 import static org.apache.logging.log4j.util.Strings.isNotBlank;
 
@@ -17,7 +21,15 @@ public class MessageHandler {
     private static final String BAD_LANGUAGE_RESPONSE_PROPERTY = "bad.language.response";
 
     public static void handleMessage(CarBot carBot, Message message) {
-        if (doesMessageContainsForbiddenWord(carBot, message.getContentDisplay())) {
+        Boolean allowForbiddenLanguage = null;
+        try {
+            allowForbiddenLanguage = Configuration.getConfiguration().getAllowForbiddenLanguage();
+        } catch (IOException e) {
+            log.error("Couldn't get configuration");
+        }
+        if (!doesServerAllowForbidenLanguage(allowForbiddenLanguage) && doesMessageContainsForbiddenWord(carBot, message.getContentDisplay())) {
+            message.addReaction(Emoji.fromFormatted("\uD83D\uDE21"))
+                    .complete();
             message.getChannel().sendMessage(carBot.getMessageSource().getMessage(BAD_LANGUAGE_RESPONSE_PROPERTY))
                     .complete();
         } else {
@@ -48,5 +60,9 @@ public class MessageHandler {
             return text.substring(0, text.indexOf(" "));
         }
         return text;
+    }
+
+    private static boolean doesServerAllowForbidenLanguage(Boolean allowForbiddenLanguage) {
+        return allowForbiddenLanguage != null && allowForbiddenLanguage;
     }
 }
